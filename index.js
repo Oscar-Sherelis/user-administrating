6d8490055a2347bae06f287af51b2c42421b1378
Oskar Šerelis <oskaras34@gmail.com>
2021-05-27 18:11:03 +0300

    Added user loading from db.json

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
-async function loadUsers (endpoint) {
-    const users = await fetch(endpoint);
-    const loadedUsers = await (await users).json();
-    let tbody = document.querySelector("tbody");
-    tbody.innerHTML = "";
+const tbodyTdClasses = [".firstname", ".lastname", ".gender", ".birthday"];
 
-    loadedUsers.forEach(user => {
-        let tbodyTr = document.createElement("tr");
-        let tdName = document.createElement("td");
-        let tdSurname = document.createElement("td");
-        let tdBirth = document.createElement("td");
-        let tdGender = document.createElement("td");
+async function loadUsers(endpoint) {
+  const users = await fetch(endpoint);
+  const loadedUsers = await (await users).json();
 
-        tdName.append(user.name);
-        tdSurname.append(user.surname);
-        tdGender.append(user.gender);
-        tdBirth.append(user.tdBirth);
+  let tbody = document.querySelector("tbody");
+  let thead = document.querySelector("thead tr");
+  thead.innerHTML = "";
+  tbody.innerHTML = "";
 
-        tbodyTr.append(tdName, tdSurname, tdGender,tdBirth)
-        tbody.append(tbodyTr);
-    })    
+  // load thead th
+  tbodyTdClasses.forEach((thName) => {
+    let th = document.createElement("th");
+    if (thName !== "id") {
+      let removedDots = thName.replace(/\./g, " ");
+      th.append(removedDots);
+      thead.append(th);
+    }
+  });
+
+  // load tbody content
+  loadedUsers.forEach((user) => {
+    let tbodyTr = document.createElement("tr");
+    let tdFirstname = document.createElement("td");
+    let tdLastname = document.createElement("td");
+    let tdBirth = document.createElement("td");
+    let tdGender = document.createElement("td");
+
+    // buttons
+    let tdButtons = document.createElement("td");
+    tdButtons.setAttribute("class", "buttons");
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "delete");
+    deleteBtn.append("DELETE");
+
+    let cancelBtn = document.createElement("button");
+    cancelBtn.setAttribute("class", "cancel");
+    cancelBtn.append("CANCEL");
+
+    let saveBtn = document.createElement("button");
+    saveBtn.setAttribute("class", "save");
+    saveBtn.append("SAVE");
+
+    tdFirstname.setAttribute("class", "firstname");
+    tdLastname.setAttribute("class", "lastname");
+    tdGender.setAttribute("class", "gender");
+    tdBirth.setAttribute("class", "birthday");
+
+    tdFirstname.append(user.firstname ?? "");
+    tdLastname.append(user.lastname ?? "");
+    tdGender.append(user.gender ?? "");
+    tdBirth.append(user.birthday ?? "");
+
+    cancelBtn.value = user.id;
+    saveBtn.value = user.id;
+    deleteBtn.value = user.id;
+
+    tdButtons.append(saveBtn, cancelBtn, deleteBtn);
+    tbodyTr.append(tdFirstname, tdLastname, tdGender, tdBirth, tdButtons);
+    tbody.append(tbodyTr);
+  });
 }
 
-loadUsers("http://localhost:3000/users")
+async function load() {
+  await loadUsers("http://localhost:3000/users");
+}
+
+load();
